2852926c352ed4ad241e1f53bee23495cd3ee74a
wangcheng <kuzidiaolewc@outlook.com>
2025-07-22 16:58:02 +0800

    init

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -22,9 +22,28 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
+static void on_menu_action_triggered(void *data)
+{
+	obs_log(LOG_INFO, "菜单项被点击")
+}
+
 bool obs_module_load(void)
 {
-	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	// 获取 OBS 前端主窗口
+	QMainWindow *main_window = (QMainWindow *)obs_frontend_get_main_window();
+
+	// 创建 QAction（菜单项）
+	QAction *menu_action = new QAction("BiliBili直播码", main_window);
+
+	// 连接 QAction 的触发信号到回调函数
+	QObject::connect(menu_action, &QAction::triggered, [](bool) {
+	    on_menu_action_triggered(nullptr);
+	});
+
+	// 将 QAction 添加到 OBS 的“工具”菜单
+	obs_frontend_add_tools_menu_qaction(menu_action);
+
+	blog(LOG_INFO, "插件加载成功，菜单已添加");
 	return true;
 }
 
