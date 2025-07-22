f68e5bb65b711e31cea1d9f984841b432184ddd3
wangcheng <kuzidiaolewc@outlook.com>
2025-07-22 17:02:39 +0800

    add frontend-api

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -18,6 +18,9 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #include <obs-module.h>
 #include <plugin-support.h>
+#include <obs-frontend-api.h>
+#include <QAction>
+#include <QMainWindow>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
