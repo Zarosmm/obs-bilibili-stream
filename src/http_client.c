6dbac21d958887b6711a6c98575936aa7bee5ad3
Zaros <kuzidiaolewc@outlook.com>
2025-07-23 17:30:22 +0800

    remove server

--- src/http_client.c
+++ src/http_client.c
@@ -94,6 +94,7 @@ HttpResponse* http_get_with_headers(const char* url, const char** headers) {
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response->status);
     curl_slist_free_all(header_list);
     curl_easy_cleanup(curl);
+    obs_log(LOG_INFO, "response %s", &response->data);
     return response;
 }
 
@@ -139,6 +140,7 @@ HttpResponse* http_post_with_headers(const char* url, const char* data, const ch
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response->status);
     curl_slist_free_all(header_list);
     curl_easy_cleanup(curl);
+    obs_log(LOG_INFO, "response %s", &response->data);
     return response;
 }
 
